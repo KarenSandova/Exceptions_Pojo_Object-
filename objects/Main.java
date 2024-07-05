7587d6ccc068743649a12a24f3a1c7191a0ccbf3
KarenSandova <sandovalk061@gmail.com>
2024-07-05 00:34:33 -0500

    Initial commit

---
+++ objects/Main.java
@@ -0,0 +1,8 @@
+package com.example.objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Karen y Jona", 19);
+        System.out.println(person);
+    }
+}
